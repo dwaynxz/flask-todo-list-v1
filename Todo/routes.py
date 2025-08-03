87676d2380e510f2d6b96eeb913c418d84aab5fa
dwaynxz <dwayne604012@gmail.com>
2025-08-03 22:59:00 +0100

    fix: removed route

--- Todo/routes.py
+++ Todo/routes.py
@@ -5,11 +5,10 @@ from flask import render_template, request
 to_do_list = todo
 
 @app.route("/")
-@app.route("home")
-@app.route("/todo-list")
+@app.route("/home")
 def home():
     return render_template("homepage.html", to_do_list=to_do_list)
 
-@app.route("/add", methods="POST")
+@app.route("/add", methods=["POST"])
 def add():
     pass
