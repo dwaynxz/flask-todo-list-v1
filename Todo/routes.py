4ec5b3cd2a686d7e9014fbd83e356f165f9927a3
dwaynxz <dwayne604012@gmail.com>
2025-08-03 22:39:41 +0100

    feat: add logic to display the items

--- Todo/routes.py
+++ Todo/routes.py
@@ -2,11 +2,13 @@ from Todo import app
 from Todo.main import todo
 from flask import render_template, request
 
+to_do_list = todo
+
 @app.route("/")
 @app.route("home")
 @app.route("todo-list")
 def home():
-    return render_template("homepage.html")
+    return render_template("homepage.html", to_do_list=to_do_list)
 
 @app.route("/add", methods="POST")
 def add():
