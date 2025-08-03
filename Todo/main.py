3645ea40c063f9f4708c26331cb1c4ed29c68e32
dwaynxz <dwayne604012@gmail.com>
2025-08-03 22:38:05 +0100

    feat: add class with methods for the list

--- Todo/main.py
+++ Todo/main.py
@@ -1,12 +1,16 @@
 class TodoList:
     def __init__(self):
-        self.to_do_list = {}
+        self.list = {}
 
     def add(self, item):
-        length_of_list = len(self.to_do_list)
-        self.to_do_list[length_of_list+1] = item
+        length_of_list = len(self.list)
+        self.list[length_of_list+1] = item
+
+    def is_empty(self):
+        if len(self.list) == 0:
+            return True
 
     def __repr__(self):
-        return f"Todo List: {self.to_do_list}"
+        return f"Todo List: {self.list}"
 
-todo = TodoList
+todo = TodoList()
