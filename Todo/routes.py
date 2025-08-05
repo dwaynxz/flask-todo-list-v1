d9a93d2d7f55cc7061081c9ae438254e491b9bdf
dwaynxz <dwayne604012@gmail.com>
2025-08-05 14:35:42 +0100

    feat: add another route and homepage.html to display the lists

--- Todo/routes.py
+++ Todo/routes.py
@@ -8,6 +8,10 @@ to_do_lists = ListManager()
 @app.route("/")
 @app.route("/home")
 def home():
+    return render_template("homepage.html", to_do_lists=to_do_lists)
+
+@app.route("/create-todo")
+def create_list():
     return render_template("create_list.html", to_do_list=to_do_list)
 
 @app.route("/add", methods=["POST"])
@@ -15,18 +19,18 @@ def add():
     task = request.form.get("task", None)
     if not task:
         flash("The Task field is empty. Please add a task", "danger")
-        return redirect("/")
+        return redirect("/create-todo")
     else:
         todo.add(task)
         flash ("Task added", category="success")
-        return redirect("/")
+        return redirect("/create-todo")
 
 @app.route("/save", methods=["POST"])
 def save():
     title = request.form.get("title")
     if not title:
         flash("Title cant be empty!", "danger")
-        return redirect("/home")
+        return redirect("/create-todo")
     else:
         to_do_lists.add_list(title=title,todolist=to_do_list)
         todo.new_list()
