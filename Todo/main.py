534d6b8bfd8ea837c37e719950178fb77a839531
dwaynxz <dwayne604012@gmail.com>
2025-08-05 23:48:25 +0100

    feat: add unique id for the todo lists

--- Todo/main.py
+++ Todo/main.py
@@ -1,6 +1,6 @@
 class TodoList:
-    def __init__(self):
-        self.list = {}
+    def __init__(self, to_do_list):
+        self.list = to_do_list
 
     def add(self, item):
         length_of_list = len(self.list)
@@ -16,7 +16,7 @@ class TodoList:
     def __repr__(self):
         return f"Todo List: {self.list}"
 
-todo = TodoList()
+todo = TodoList({})
 
 class ListManager:
     def __init__(self):
@@ -27,7 +27,13 @@ class ListManager:
             return True
 
     def add_list(self, title, todolist):
-        self.lists[title] = todolist
+        id_ = len(self.lists)+1
+        self.lists[(id_, title)] = todolist
+
+    def get_key_by_id(self, id_):
+        for key in self.lists.keys():
+            if id_ in key:
+                return key
 
     def __repr__(self):
         return f"Lists: {self.lists}"
